87b42e2b57cafc9e5f13a860b351cd75cda51a32
xValtor <valtorhal@yandex.ru>
2025-06-23 14:44:55 +0300

    Создание проекта

---
+++ src/test/java/org/store/webstore/WebStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.store.webstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
